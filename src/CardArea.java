845dba57761d87ca24da1ebbe0b22c0970684ff5
BT_1E10_23 <sample@bths.edu>
2022-03-15 13:55:37 -0400

    Initial commit

---
+++ src/CardArea.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class CardArea {
+
+    private ArrayList<String> board;
+
+    public CardArea ()
+    {
+        board = new ArrayList<String>();
+    }
+
+    public void setBoard()
+    {
+        
+    }
+}
---
+++ src/MemoryRunner.java
@@ -0,0 +1,2 @@
+public class MemoryRunner {
+}
