b5142d0671996de6e012ce0e468294703dee365e
J <jayl9@nycstudents.net>
2022-03-23 21:06:13 -0400

    First Try

--- src/CardArea.java
+++ src/CardArea.java
@@ -1,16 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CardArea {
 
-    private ArrayList<String> board;
+    private String[][] board;
+    private String[] cardNames;
+    private ArrayList<String> pokemons;
 
     public CardArea ()
     {
-        board = new ArrayList<String>();
+        importPokemonNames();
+        board = new String[6][6];
+
     }
 
-    public void setBoard()
+    private void importPokemonNames()
     {
-        
+        String[] tmp = null;
+        try
+        {
+            FileReader fileReader = new FileReader("src\\PokemonNames.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ArrayList<String> lines = new ArrayList<String>();
+            String line = null;
+
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                lines.add(line);
+            }
+
+            bufferedReader.close();
+            tmp = lines.toArray(new String[lines.size()]);
+            System.out.println("\nFile imported successfully!");
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error importing file; unable to access "+ e.getMessage());
+        }
+
+        pokemons = new ArrayList<String>(Arrays.asList(tmp));
     }
+
+
 }
--- src/MemoryRunner.java
+++ src/MemoryRunner.java
@@ -1,2 +1,5 @@
 public class MemoryRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
